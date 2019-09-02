4bb72252aed64fcd12e571835c5f6d32527e7e0e
Boxing199 <virkolomeo@gmail.com>
2019-09-02 22:07:00 +0300

    Updates

---
+++ precache-manifest.651707cc4300c6608021af1ef5922609.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4735e33e43e54456f34e32d4adeb6fa2",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "74853e1f65dbe6fd9d50",
+    "url": "/chat/static/css/main.63430899.chunk.css"
+  },
+  {
+    "revision": "232b9386e884b20fd397",
+    "url": "/chat/static/js/2.3d7f4cff.chunk.js"
+  },
+  {
+    "revision": "74853e1f65dbe6fd9d50",
+    "url": "/chat/static/js/main.b5398831.chunk.js"
+  },
+  {
+    "revision": "2878249f4c4ae1237cb7",
+    "url": "/chat/static/js/runtime~main.eff7a0c2.js"
+  },
+  {
+    "revision": "315ddeed170698edadfee7c6a18dd420",
+    "url": "/chat/static/media/Spinner.315ddeed.svg"
+  },
+  {
+    "revision": "0a5e7a1fa5a0c49f27a55ab285e7d375",
+    "url": "/chat/static/media/avatar.0a5e7a1f.png"
+  }
+]);
